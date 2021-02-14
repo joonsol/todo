2551dcd16bb379c6201a2a599ffb0178e3c0a563
joonsol <lizlove0523@gmail.com>
2021-02-14 16:11:19 +0900

    first components setting

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todoHeader></todoHeader>
+    <todoInput></todoInput>
+    <todoList></todoList>
+    <todoFooter></todoFooter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import todoHeader from './components/todoHeader.vue'
+import todoInput from './components/todoInput.vue'
+import todoList from './components/todoList.vue'
+import todoFooter from './components/todoFooter.vue'
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    todoHeader:todoHeader,
+    todoInput:todoInput,
+    todoList:todoList,
+    todoFooter:todoFooter,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
